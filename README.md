179c0b5dfea165e43753abc4632eb2e99933689c
kite <2257440643@qq.com>
2024-05-15 21:51:50 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,46 @@
+# generator-smiley [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+
+> Personal scaffolding
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-smile using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-smile
+```
+
+Then generate your new project:
+
++ Vue2 Project
+
+```bash
+yo smiley:vue2-template <project_name>
+```
+
++ Vue3 Project
+
+```bash
+yo smiley:vue3-template <project_name>
+```
+
+## Getting To Know Yeoman
+
+* Yeoman has a heart of gold.
+* Yeoman is a person with feelings and opinions, but is very easy to work with.
+* Yeoman can be too opinionated at times but is easily convinced not to be.
+* Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+© [Smiley](https://github.com/Kitesource)
+
+[npm-image]: https://badge.fury.io/js/generator-smile.svg
+[npm-url]: https://npmjs.org/package/generator-smile
+[travis-image]: https://travis-ci.com//generator-smile.svg?branch=master
+[travis-url]: https://travis-ci.com//generator-smile
+[daviddm-image]: https://david-dm.org//generator-smile.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org//generator-smile
+[coveralls-image]: https://coveralls.io/repos//generator-smile/badge.svg
+[coveralls-url]: https://coveralls.io/r//generator-smile
---
+++ generators/vue2-template/index.js
@@ -0,0 +1,92 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.argument("name", {
+      type: String,
+      required: false,
+      desc: "项目名称/Project Name"
+    });
+    // 项目根目录
+    this.destinationRoot(this.options.name || "");
+  }
+
+  getTemplateData() {
+    return Object.assign(this.props, {
+      name: this.options.name
+    });
+  }
+
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(`Welcome to the wicked ${chalk.red("generator-smiley")} generator!`)
+    );
+
+    const prompts = [
+      {
+        type: "list",
+        name: "packagesManager",
+        message: "包管理器",
+        default: "yarn",
+        choices: [
+          {
+            value: "npm",
+            name: "NPM"
+          },
+          {
+            value: "yarn",
+            name: "Yarn"
+          }
+        ]
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  writing() {
+    /** 全量渲染模板文件 */
+    const templateData = this.getTemplateData();
+    this.renderTemplate("**/*", this.destinationRoot(), templateData);
+    this.renderTemplate("**/.*", this.destinationRoot(), templateData);
+  }
+
+  install() {
+    try {
+      switch (this.props.packagesManager) {
+        case "yarn":
+          this.spawnCommandSync("yarn");
+          break;
+        case "npm":
+          this.spawnCommandSync("npm", ["i"]);
+          break;
+        default:
+          this.spawnCommandSync("yarn");
+          break;
+      }
+    } catch (e) {
+      console.log("check error:", e);
+    }
+  }
+
+  end() {
+    const { packagesManager: manager, name } = this.props;
+    const runCommmand = `${manager} ${manager === "npm" ? "run" : ""} serve`;
+    this.log(
+      yosay(
+        `🎉 Successfully created project ${chalk.blue(name)}.
+        👉  Get started with the following commands:
+        ${chalk.blue(name ? `cd ${name}` : "current directory")}
+        ${chalk.blue(runCommmand)}`
+      )
+    );
+  }
+};
